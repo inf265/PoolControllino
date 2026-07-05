a97f7c37335bc6f2cc4d22a0ffdc5d74de19ab03
Frederic-Philippe  Metz <metz@myfred.net>
2026-07-05 14:02:48 +0200

    Version info

--- version.h
+++ version.h
@@ -2,12 +2,17 @@
 
 #include <Controllino.h>
 
-#define _STRINGIZE(x) #x
-#define STRINGIZE(x) _STRINGIZE(x)
+// GIT_VERSION is written at BUILD time by cmake/git_version.cmake into
+// build/generated/version_git.h, so it always matches the flashed firmware
+// (including a "-dirty" suffix when built from uncommitted changes).
+#if defined(__has_include)
+#  if __has_include("version_git.h")
+#    include "version_git.h"
+#  endif
+#endif
 
-// the next few macros will put the current git "stamp" into the code
-#ifdef GITVERSION
-static const char *gitVersion = STRINGIZE(GITVERSION);
+#ifdef GIT_VERSION
+static const char *gitVersion = GIT_VERSION;
 #else
 static const char *gitVersion = "No git version specified";
 #endif
